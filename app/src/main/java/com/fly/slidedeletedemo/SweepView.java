5b3d9b9285276a39bbe8538627bccd9e1ac73381
FlyStorm <1070449522@qq.com>
2019-05-01 09:04:12 +0800

    touchup的实现：当触摸的是contentview时，向左滑动，如果滑动的距离大于deleteview距离的一半的话，就把deleteview完全显示；如果滑动的距离小于或者等于deleteview距离一半的话，就把deleteview完全隐藏。当滑动delteview时，也是这样的。

--- app/src/main/java/com/fly/slidedeletedemo/SweepView.java
+++ app/src/main/java/com/fly/slidedeletedemo/SweepView.java
@@ -72,40 +72,60 @@ public class SweepView extends ViewGroup {
         return true;
     }
 
-    class MyDragCallback extends ViewDragHelper.Callback{
+    class MyDragCallback extends ViewDragHelper.Callback {
 
         /**
-         *
          * @param view 是的是touch的view，不是自己，是自己的孩子
-         * @param i 点的标记
+         * @param i    点的标记
          * @return viewdraghelper是否继续分析处理child的相关touch事件
          */
         @Override
         public boolean tryCaptureView(@NonNull View view, int i) {
             System.out.println("调用tryCaptureView");
-            System.out.println("触摸的是否是内容控件，contentview:"+(mContentView==view));
-            System.out.println("触摸的是否是删除控件，deleteview:"+(mDeleteView==view));
-            return mContentView==view || mDeleteView==view;
+            System.out.println("触摸的是否是内容控件，contentview:" + (mContentView == view));
+            System.out.println("触摸的是否是删除控件，deleteview:" + (mDeleteView == view));
+            return mContentView == view || mDeleteView == view;
         }
 
         /**
          * 捕获水平方向移动的位移数据，此方向调用后，view就会动了
+         *
          * @param child 是哪个孩子移动了
-         * @param left 父容器的左侧
-         * @param dx 增量值
+         * @param left  父容器的左侧
+         * @param dx    增量值
          * @return 你想我怎么动
          */
         @Override
         public int clampViewPositionHorizontal(@NonNull View child, int left, int dx) {
+            //处理拖动contentview和deleteview越界的问题
+            if (child == mContentView) {
+                //假如拖动的是contentview
+                if (left > 0) {
+                    //如果向右拖动话，就不让它拖动
+                    return 0;
+                } else {
+                    if (-left > mDeleteView.getWidth()) {
+                        return -mDeleteView.getWidth();
+                    }
+                }
+            } else if (child == mDeleteView) {
+                //假如拖动的是deleteview的话
+                if (left < mContentView.getWidth() - mDeleteView.getWidth()) {
+                    return mContentView.getWidth() - mDeleteView.getWidth();
+                } else if (left > mContentView.getWidth()) {
+                    return mContentView.getWidth();
+                }
+            }
             return left;
         }
 
         /**
          * 当view的位置改变的时候回调
+         *
          * @param changedView 哪个view的位置改变了
-         * @param left changedview的left
+         * @param left        changedview的left
          * @param top
-         * @param dx 增量值
+         * @param dx          增量值
          * @param dy
          */
         @Override
@@ -114,26 +134,65 @@ public class SweepView extends ViewGroup {
             invalidate();
 
             //contentview的宽度
-            int contentviewWidth=mContentView.getWidth();
-            int deleteviewWidth=mDeleteView.getWidth();
-            int deleteviewHeight=mDeleteView.getHeight();
+            int contentviewWidth = mContentView.getWidth();
+            int deleteviewWidth = mDeleteView.getWidth();
+            int deleteviewHeight = mDeleteView.getHeight();
 
-            if (changedView==mContentView){
+            if (changedView == mContentView) {
                 //如果contentview的位置改变了，相应地就需要改变deleteview的位置
-                int devLeft=contentviewWidth+left;
-                int devRight=contentviewWidth+left+deleteviewWidth;
-                mDeleteView.layout(devLeft,0,devRight,deleteviewHeight);
-            }else{
+                int devLeft = contentviewWidth + left;
+                int devRight = contentviewWidth + left + deleteviewWidth;
+                mDeleteView.layout(devLeft, 0, devRight, deleteviewHeight);
+            } else {
                 //如果deleteview位置改变了，就需要改变contentview的位置
-                int conLeft=left-contentviewWidth;
-                int conRight=left;
-                mContentView.layout(conLeft,0,left,deleteviewHeight);
+                int conLeft = left - contentviewWidth;
+                int conRight = left;
+                mContentView.layout(conLeft, 0, left, deleteviewHeight);
             }
         }
 
+        /**
+         * 当触发touchup的时候回调
+         * @param releasedChild 是哪个view的touchup触发了
+         * @param xvel x方向的速率
+         * @param yvel y方向的速率
+         */
         @Override
         public void onViewReleased(@NonNull View releasedChild, float xvel, float yvel) {
-            super.onViewReleased(releasedChild, xvel, yvel);
+            int deleteWidth = mDeleteView.getMeasuredWidth();
+            int contentWidth = mContentView.getMeasuredWidth();
+            int deleteHeight = mDeleteView.getMeasuredHeight();
+            /*//如果contentview的touchup触发了
+            if (releasedChild==mContentView){
+                //向左滑动，滑动的距离大于deleteview的宽度一半
+                if (-mContentView.getLeft()>deleteWidth/2){
+                    //显示deleteview
+                    mDeleteView.layout(contentWidth-deleteWidth,0,contentWidth,deleteHeight);
+                    //显示contentview
+                    mContentView.layout(-deleteWidth,0,contentWidth-deleteWidth,deleteHeight);
+                }else{
+                    //向左滑动，滑动的距离小于或者等于deleteview宽度的一半
+                    //显示deleteview
+                    mDeleteView.layout(contentWidth,0,contentWidth+deleteWidth,deleteHeight);
+                    //显示contentview
+                    mContentView.layout(0,0,contentWidth,deleteHeight);
+                }
+            }*/
+
+            //不管是滑动contentview，还是滑动deleteview都是一样的
+            //向左滑动，滑动的距离大于deleteview的宽度一半
+            if (-mContentView.getLeft()>deleteWidth/2){
+                //显示deleteview
+                mDeleteView.layout(contentWidth-deleteWidth,0,contentWidth,deleteHeight);
+                //显示contentview
+                mContentView.layout(-deleteWidth,0,contentWidth-deleteWidth,deleteHeight);
+            }else{
+                //向左滑动，滑动的距离小于或者等于deleteview宽度的一半
+                //显示deleteview
+                mDeleteView.layout(contentWidth,0,contentWidth+deleteWidth,deleteHeight);
+                //显示contentview
+                mContentView.layout(0,0,contentWidth,deleteHeight);
+            }
         }
     }
 }
